a30a2bbbe6b6b7741d297f35028da07999e378db
Hardikkumar <dhadukhardik111@gmail.com>
2019-07-24 14:26:24 +0530

    Update Read into Python.py

--- Read into Python.py
+++ Read into Python.py
@@ -1,17 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import joblib
 from sklearn.feature_extraction.text import CountVectorizer
 
-
-# In[2]:
-
-
 data = pd.read_csv('google_play_store_apps_reviews_training.csv')
-
